e7f13475d15d9a6cce7fced621ae12bbd70725c8
Dayo <dayo@dojah.io>
2022-12-06 03:25:36 +0100

    added pagination

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,23 @@
 import {Fragment} from 'react';
 import {Dialog, Transition} from '@headlessui/react';
 
+interface TransitionParent {
+  children: React.ReactNode;
+  className: string;
+}
+
+function TransitionParent({children, className}: TransitionParent) {
+  return <div className={className}>{children}</div>;
+}
+
 interface ModalProps {
   isOpen: boolean;
   children: React.ReactNode;
   className?: string;
   onClose: () => void;
+  padding?: string;
+  transitionParentClassName?: string;
+  navAnimation?: boolean;
 }
 
 export default function Modal({
@@ -13,6 +25,8 @@ export default function Modal({
   children,
   className,
   onClose,
+  transitionParentClassName,
+  navAnimation,
 }: ModalProps) {
   return (
     <>
@@ -29,17 +43,30 @@ export default function Modal({
           >
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
-
           <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex h-full min-h-screen items-start justify-center p-[12vh] text-center backdrop-blur-sm">
+            <TransitionParent
+              className={`flex h-full min-h-screen items-start justify-center px-4 text-center backdrop-blur-sm ${
+                transitionParentClassName
+                  ? transitionParentClassName
+                  : 'pt-20 md:p-[12vh]'
+              }`}
+            >
               <Transition.Child
                 as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
+                enter={`${
+                  navAnimation
+                    ? 'ease-in-out duration-500'
+                    : 'ease-out duration-300'
+                }`}
+                enterFrom={`opacity-0 ${
+                  navAnimation ? '-translate-y-10' : 'scale-95'
+                }`}
+                enterTo={`opacity-100 ${navAnimation ? '' : 'scale-100'}`}
+                leave="ease-in duration-300"
                 leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
+                leaveTo={`opacity-0 ${
+                  navAnimation ? ' translate-y-20' : 'scale-95'
+                }`}
               >
                 <Dialog.Panel
                   className={`${className} flex w-full transform flex-col text-left align-middle transition-all`}
@@ -47,7 +74,7 @@ export default function Modal({
                   {children}
                 </Dialog.Panel>
               </Transition.Child>
-            </div>
+            </TransitionParent>
           </div>
         </Dialog>
       </Transition>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,6 @@ module.exports = {
       transitionProperty: {
         outlineOffset: 'outline-offset',
       },
-      gridTemplateColumns: {
-        card: 'repeat(auto-fit, minmax(300px, 1fr))',
-      },
     },
     colors: {
       body: '#53504B',
